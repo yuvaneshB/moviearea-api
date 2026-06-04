bdb53d512cf6401e1683b5f13d1453e7c4790456
Yuvanesh B <yuvanesh7798@gmail.com>
2026-06-04 11:47:18 +0530

    route structure

--- main.js
+++ main.js
@@ -7,6 +7,26 @@ app.get('/', (req, res)=>{
     res.json({message: "hello guys"})
 });
 
+// CRUD functionality of movies
+// get-for Reading
+app.get('/movies', ()=>{
+    
+});
+
+//post- for Creating
+app.post('/movies', ()=>{
+
+});
+
+//put- for Updating
+app.put('/movies/:id', ()=>{
+
+});
+
+//delete- for Delete
+app.delet('/movies/:id', ()=>{
+
+});
 app.listen(PORT, ()=>{
     console.log(`The server is running at http://localhost:${PORT}`)
 })
